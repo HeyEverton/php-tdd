406a111815033b465e3b29eb0b5f6f914d6e5017
Everton <everton.h.g.fonseca@gmail.com>
2022-10-31 13:57:35 -0300

    pulando teste e marcando como incompleto

--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -6,26 +6,52 @@ use PHPUnit\Framework\TestCase;
 
 class CarrinhoTest extends TestCase
 {
-    public function testSeClasseCarrinhoExiste()
+    private $carrinho;
+    private $produto;
+
+    public function setUp() : void
+    {
+        $this->carrinho = new Carrinho();
+        $this->produto = new Produto();
+    }
+
+    public function tearDown() : void
+    {
+        unset($this->carrinho);
+        unset($this->produto);
+    }
+
+    // public function testSeClasseCarrinhoExiste()
+    // {
+    //     $classe = class_exists('\\Code\\Carrinho');
+    //     $this->assertTrue($classe);
+    // }
+
+    protected function assertPreConditions() : void
     {
         $classe = class_exists('\\Code\\Carrinho');
         $this->assertTrue($classe);
     }
 
+    protected function assertPosConditions() : void
+    {
+        
+    }
+
     public function testAdicaoDeProdutosNoCarrinho()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setName('Produto Teste');
         $produto->setPrice(19.99);
         $produto->setSlug('produto-teste');
 
-        $produto2 = new Produto();
+        $produto2 = $this->produto;
         $produto2->setName('Produto Teste2');
         $produto2->setPrice(19.90);
         $produto2->setSlug('produto-teste2');
 
 
-        $carrinho = new Carrinho();
+        $carrinho = $this->carrinho;
         $carrinho->addProduto($produto);
         $carrinho->addProduto($produto2);
 
@@ -36,12 +62,12 @@ class CarrinhoTest extends TestCase
 
     public function testSeValoresDeProdutosNoCarrinhoEstaoCorretosConformePassados()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setName('Produto Teste');
         $produto->setPrice(19.99);
         $produto->setSlug('produto-teste');
 
-        $carrinho = new Carrinho();
+        $carrinho = $this->carrinho;
         $carrinho->addProduto($produto);
 
         $this->assertEquals('Produto Teste', $carrinho->getProdutos()[0]->getName());
@@ -51,23 +77,38 @@ class CarrinhoTest extends TestCase
 
     public function testeSeTotalDeProdutosEValorDaCompraEstaoCorretos()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setName('Produto Teste');
         $produto->setPrice(19.99);
         $produto->setSlug('produto-teste');
 
-        $produto2 = new Produto();
+        $produto2 = $this->produto;
         $produto2->setName('Produto Teste2');
-        $produto2->setPrice(19.90);
+        $produto2->setPrice(19.99);
         $produto2->setSlug('produto-teste2');
 
 
-        $carrinho = new Carrinho();
+        $carrinho = $this->carrinho;
         $carrinho->addProduto($produto);
         $carrinho->addProduto($produto2);
 
         $this->assertEquals(2, $carrinho->getTotalProdutos());
-        $this->assertEquals(39.89, $carrinho->getTotalCompra());
+        $this->assertEquals(39.98, $carrinho->getTotalCompra());
         
+    }
+
+    public function testIncompleto()
+    {
+        $this->assertTrue(true);
+        $this->markTestIncomplete('Este teste ainda não está completo');
+    }
+
+    public function testSeFeatureEspecificaParaVersaoPHP81TrabalhaDeFormaEsperada()
+    {
+        if(PHP_VERSION != 8.1) {
+            $this->markTestSkipped('Este teste foi pulado');
+        }
+        
+        $this->assertTrue(true);
     }
 }
--- tests/ProdutoTest.php
+++ tests/ProdutoTest.php
@@ -5,9 +5,25 @@ use PHPUnit\Framework\TestCase;
 
 class ProdutoTest extends TestCase
 {
+    private $produto;
+    public function setUp() : void
+    {
+        $this->produto = new Produto();
+    }
+
+    // public static function setUpBeforeClass() : void
+    // {
+    //     print __CLASS__;
+    // }
+
+    // public static function tearDownAfterClass() : void
+    // {
+    //     print __METHOD__;
+    // }
+
     public function testSeONomeDoProdutoESetadoCorretamente()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setName('Produto 1');
         
         $this->assertEquals('Produto 1', $produto->getName(), 'Valores não são iguais');
@@ -15,7 +31,7 @@ class ProdutoTest extends TestCase
     }
     public function testSeOPrecoDoProdutoESetadoCorretamente()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setPrice('19,99');
         
         $this->assertEquals('19,99', $produto->getPrice(), 'Valores não são iguais');
@@ -23,7 +39,7 @@ class ProdutoTest extends TestCase
     }
     public function testSeOSlugDoProdutoESetadoCorretamente()
     {
-        $produto = new Produto();
+        $produto = $this->produto;
         $produto->setSlug('produto-1');
         
         $this->assertEquals('produto-1', $produto->getSlug(), 'Valores não são iguais');
